d2f56a5cb9672fa0dea5150c5d69217051bc4775
Manveer695 <singhmanveer1995@gmail.com>
2017-03-16 17:50:41 +0530

    Create viberReq.js

---
+++ viberReq.js
@@ -0,0 +1,24 @@
+
+  var url = "https://chatapi.viber.com/pa/set_webhook?callback=?";
+                    
+  var events = [];
+  function fn(json){
+    console.log(json);
+  }
+    
+    $.ajax({
+    url : url ,
+    dataType : "jsonp",
+    type : 'POST',
+    jsonpCallback: 'fn',
+    headers: {
+    	'X-Viber-Auth-Token': '45a53f0fcb325002-41552d1f93cd0d0f-1a8d7fa78758d158',
+    },
+    success : function (data) {
+      //console.log(data);
+    },
+    error : function (data, errorThrown) {
+      //console.log(data);
+      alert(errorThrown);
+    }
+});
