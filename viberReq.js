6bd1489c59e4b56ada693c0d6027a870735ef816
Manveer695 <singhmanveer1995@gmail.com>
2017-03-17 12:11:29 +0530

    Update viberReq.js

--- viberReq.js
+++ viberReq.js
@@ -12,7 +12,7 @@
     type : 'POST',
     jsonpCallback: 'fn',
     headers: {
-    	'X-Viber-Auth-Token': '45a53f0fcb325002-41552d1f93cd0d0f-1a8d7fa78758d158',
+    	'X-Viber-Auth-Token': '45a53f0fcb325002-41552d1f93cd0d0f-1a8d7fa78758d158'
     },
     success : function (data) {
       //console.log(data);
