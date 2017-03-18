42b6e42f09b7420f7831e9c8eaf7a00468b6c8ec
Manveer695 <singhmanveer1995@gmail.com>
2017-03-18 22:30:47 +0530

    adding data field

--- viberReq.js
+++ viberReq.js
@@ -11,7 +11,12 @@
     dataType : "jsonp",
     type : 'POST',
     jsonpCallback: 'fn',
-    'X-Viber-Auth-Token': '45a53f0fcb325002-41552d1f93cd0d0f-1a8d7fa78758d158',
+    data : {  
+      "url": "https://my.host.com",
+      'X-Viber-Auth-Token': '45a53f0fcb325002-41552d1f93cd0d0f-1a8d7fa78758d158',
+      "event_types": ["delivered", "seen", "failed", "subscribed", "unsubscribed", "conversation_started"]  
+    },
+    
     success : function (data) {
       //console.log(data);
     },
