8ff09d9feabc5e0acae80d4c8a253c1bbd726e12
Manveer695 <singhmanveer1995@gmail.com>
2017-03-19 01:56:43 +0530

    adding headers field
    Also some changes in data field.

--- viberReq.js
+++ viberReq.js
@@ -1,18 +1,23 @@
 
-  var url = "https://chatapi.viber.com/pa/set_webhook";
+ var url = "https://chatapi.viber.com/pa/set_webhook?callback=?";
                     
   var events = [];
+  function fn(json){
+    console.log(json);
+  }
     
     $.ajax({
     url : url ,
     dataType : "jsonp",
     type : 'POST',
-    data : {  
-      "url": "https://my.host.com",
-      'X-Viber-Auth-Token': '45a53f0fcb325002-41552d1f93cd0d0f-1a8d7fa78758d158',
-      "event_types": ["delivered", "seen", "failed", "subscribed", "unsubscribed", "conversation_started"]  
+    jsonpCallback: 'fn',
+    headers: {
+    	'X-Viber-Auth-Token': '45a53f0fcb325002-41552d1f93cd0d0f-1a8d7fa78758d158'
     },
-    
+    data : {  
+    "url": "https://my.host.com",
+    "event_types": ["delivered", "seen", "failed", "subscribed", "unsubscribed", "conversation_started"]  
+    },
     success : function (data) {
       //console.log(data);
     },
