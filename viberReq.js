50538e2081f73681d8a54dc86c5d1a5a7981c45b
Manveer695 <singhmanveer1995@gmail.com>
2017-03-19 22:42:14 +0530

    adding origin header

--- viberReq.js
+++ viberReq.js
@@ -1,28 +1,25 @@
 
- var url = "https://chatapi.viber.com/pa/set_webhook?callback=?";
+ var url = "https://chatapi.viber.com/pa/set_webhook";
                     
   var events = [];
-  function fn(json){
-    console.log(json);
-  }
     
     $.ajax({
     url : url ,
-    dataType : "jsonp",
+    dataType : 'json',
     type : 'POST',
-    jsonpCallback: 'fn',
     headers: {
-    	'X-Viber-Auth-Token': '45a53f0fcb325002-41552d1f93cd0d0f-1a8d7fa78758d158'
+     'X-Viber-Auth-Token': '45a53f0fcb325002-41552d1f93cd0d0f-1a8d7fa78758d158',
+     'Origin': 'https://manveer695.github.io/hello-world/viberStuff.html'
     },
     data : {  
-    "url": "https://my.host.com",
-    "event_types": ["delivered", "seen", "failed", "subscribed", "unsubscribed", "conversation_started"]  
+     "url": "https://manveer695.github.io/hello-world/viberStuff.html",
+     "event_types": ["delivered", "seen", "failed", "subscribed", "unsubscribed", "conversation_started"]  
     },
     success : function (data) {
       //console.log(data);
     },
     error : function (data, errorThrown) {
-      //console.log(data);
-      alert(errorThrown);
+     //console.log(data);
+     alert(errorThrown);
     }
 });
