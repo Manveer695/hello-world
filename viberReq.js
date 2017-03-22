f054891cdc34dc97300716fbae79253aaa16f189
Manveer695 <singhmanveer1995@gmail.com>
2017-03-22 10:48:21 +0530

    The fields required for postman removed.

--- viberReq.js
+++ viberReq.js
@@ -1,21 +1,16 @@
-var url = "https://chatapi.viber.com/pa/set_webhook";
-     
-     $.ajax({
-     url : url ,
-     dataType : "json",
-     type : 'POST',
-     data : {  
-      "url": "https://manveer695.github.io/hello-world/viberStuff.html",
-      "event_types": ["delivered", "seen", "failed", "subscribed", "unsubscribed", "conversation_started"]  
-     },
-     beforeSend: function(request) {
-         request.setRequestHeader('X-Viber-Auth-Token', '45a53f0fcb325002-41552d1f93cd0d0f-1a8d7fa78758d158');
-     },
-     success : function (data) {
-       console.log(data);
-     },
-     error : function (data, errorThrown) {
-       //console.log(data);
-       alert(errorThrown);
-     }
-  });
+var settings = {
+  "async": true,
+  "crossDomain": true,
+  "url": "https://chatapi.viber.com/pa/send_message",
+  "method": "POST",
+  "headers": {
+    "x-viber-auth-token": "45a53f0fcb325002-41552d1f93cd0d0f-1a8d7fa78758d158",
+    "content-type": "application/json",
+    "cache-control": "no-cache"
+  },
+  "data": "{ \r\n\"url\": \"https://maveer695.github.io/hello-world/viberStuff.html\", \r\n\"event_types\": [\"delivered\", \"seen\", \"failed\", \"subscribed\", \"unsubscribed\", \"conversation_started\"] \r\n}"
+}
+
+$.ajax(settings).done(function (response) {
+  console.log(response);
+});
